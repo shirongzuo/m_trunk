1ab1126898dd05fa5dda752b744cf9de3163adb8
shirongzuo <shilingggg@gmail.com>
2019-12-11 10:58:58 +1100

    process subfolders version 2

--- measure_dots_v1.m
+++ measure_dots_v1.m
@@ -4,62 +4,72 @@ path = 'Z:\m_trunk\test images\';
 multipleRecord = {};
 record_index = 1;
 
-% D = dir;
-% dFlags = [D.isdir] & ~strcmp({D.name},'.') & ~strcmp({DS.name},'..');
-% now = D(dirFlags);
+files_current_folder = dir;
+dirFlags = [files_current_folder.isdir] & ~strcmp({files_current_folder.name},'.') & ~strcmp({files_current_folder.name},'..');
+current_subfolders = files_current_folder(dirFlags);
 
-% search through all folders
-files = dir('Z:\m_trunk\test images\c14');
-dirFlags = [files.isdir] & ~strcmp({files.name},'.') & ~strcmp({files.name},'..');
-subFolders = files(dirFlags);
-
-for k = 1 : length(subFolders)
-  fprintf('Sub folder #%d = %s\n', k, subFolders(k).name);
-  folderName = subFolders(k).name;
-  secondPath = strcat('Z:\m_trunk\test images\c14\', folderName);
-  secondPath = strcat(secondPath, '\');
-  cd(secondPath);
-  
-    % inside each subfolder...
-    imgNames = dir([secondPath,'*.JPG']);  
-%     multipleRecord = {};
-
-    for i=1:length(imgNames)
-        name = imgNames(i).name;
-        I=imread([secondPath,name]);
-        im=I(:,:,1);
-        im=im(25:end,25:end);
-        Dot_valid = dots_process(im,1);
-        pause(1);
-
-        % Ark
-        % write output to result file
-        Dot_size=[];
-        Dot_intensity=[];
-        for j=1:length(Dot_valid)
-            Dot_size = [Dot_size,Dot_valid(j).size];
-            Dot_intensity=[Dot_intensity,Dot_valid(j).intensity];
-        end
-
-        % write each record to file
-        entry = int2str(record_index);
-        imgName = name;
-
-        imgID = 'imgID';
-        imgNumber = int2str(length(Dot_valid));
-        imgIntensity = int2str(median(Dot_intensity));
-        imgAboveThres = int2str(10);
-        imgSize = int2str(median(Dot_size));
-
-        singleRecordStr = [entry ',' folderName ',' imgName ',' imgNumber ',' imgIntensity ',' imgSize];
-        multipleRecord = [multipleRecord, singleRecordStr];
-        record_index = record_index+1;
-    end 
+for v = 1 : length(current_subfolders)
+    fprintf('Sub folder #%d = %s\n', v, current_subfolders(v).name);
     
-    cd ..
-  
-end
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % search through all folders
+    % files = dir('Z:\m_trunk\test images\c14');
+    
+    second_dir = strcat(path, current_subfolders(v).name);
+    cd(second_dir);
+    files = dir(second_dir);
+    
+    dirFlags = [files.isdir] & ~strcmp({files.name},'.') & ~strcmp({files.name},'..');
+    subFolders = files(dirFlags);
 
+    for k = 1 : length(subFolders)
+      fprintf('Sub folder #%d = %s\n', k, subFolders(k).name);
+      folderName = subFolders(k).name;
+      secondPath = strcat(second_dir, '\');
+      secondPath = strcat(secondPath, folderName);
+      secondPath = strcat(secondPath, '\');
+      cd(secondPath);
+
+        % inside each subfolder...
+        imgNames = dir([secondPath,'*.JPG']);  
+    %     multipleRecord = {};
+
+        for i=1:length(imgNames)
+            name = imgNames(i).name;
+            I=imread([secondPath,name]);
+            im=I(:,:,1);
+            im=im(25:end,25:end);
+            Dot_valid = dots_process(im,1);
+            pause(1);
+
+            % Ark
+            % write output to result file
+            Dot_size=[];
+            Dot_intensity=[];
+            for j=1:length(Dot_valid)
+                Dot_size = [Dot_size,Dot_valid(j).size];
+                Dot_intensity=[Dot_intensity,Dot_valid(j).intensity];
+            end
+
+            % write each record to file
+            entry = int2str(record_index);
+            imgName = name;
+
+            imgID = 'imgID';
+            imgNumber = int2str(length(Dot_valid));
+            imgIntensity = int2str(median(Dot_intensity));
+            imgAboveThres = int2str(10);
+            imgSize = int2str(median(Dot_size));
+
+            singleRecordStr = [entry ',' folderName ',' imgName ',' imgNumber ',' imgIntensity ',' imgSize];
+            multipleRecord = [multipleRecord, singleRecordStr];
+            record_index = record_index+1;
+        end 
+        cd ..
+    end
+    cd ..
+end
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % imgNames = dir([path,'*.JPG']);
 
@@ -96,6 +106,7 @@ end
 %     multipleRecord = [multipleRecord, singleRecordStr];
 % end
 
+% after processing all the subfolders, write result to file
 fileID = fopen('data.csv', 'a');
 fprintf(fileID, 'Entry,Folder,Name,Number,Intensity,Size\n');
 for i=1:length(multipleRecord)
